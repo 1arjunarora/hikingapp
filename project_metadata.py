2d4f5a7006ee0574407a98f46bc73d77822ad986
Arjun Arora <32399220+1arjunarora@users.noreply.github.com>
2022-05-10 15:17:42 -0700

    Update project_metadata.py
    Updated deployment

--- project_metadata.py
+++ project_metadata.py
@@ -1,6 +1,6 @@
 import streamlit as st  # willl be redundant in main program
 
-DEPLOYMENT_ID = '6272eac547441ad43bfed4b0'
+DEPLOYMENT_ID = '627ae41b92fa5b4c76f441b2'
 API_URL = 'https://cfds-ccm-prod.orm.datarobot.com/predApi/v1.0/deployments/{deployment_id}/predictions'    # noqa
 API_KEY = 'NjI1NzkxY2ViNmE0NzJjOGVkZjhmNGNkOjczY3BhVm8vQmR3OUgxR3BCa3FleHdRMGl4dys0T1JNdk1KMGswTzRrK1U9'
 DATAROBOT_KEY = '544ec55f-61bf-f6ee-0caf-15c7f919a45d'
